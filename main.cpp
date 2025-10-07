1b7c161b01c609740eb3cce996d7fd45f5c2599f
Aditi-Sharma13 <aditisharmasrbps@gmail.com>
2025-10-07 19:06:44 +0530

    classe

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
 class student{
+    int roll;
+    float marks;
+    string name;
+    string course;
+
     public:
-    void add(){
+
+    void input(){
+        cout<<"ENTER ROLL NO :";
+        cin>>roll;
+        cout<<"ENTER YOUR NAME :";
+        cin>>name;
+        cout<<"ENTER MARKS :";
+        cin >>marks;
+        cout<<"ENTER COURSE NAME :";
+        cin>>course;
         
+    }
+    void display(){
+        cout<<"\nROLL NO."<<roll;
+        cout<<"\nNAME"<<name;
+        cout<<"\nMARKS"<<marks;
+        cout<<"\nCOURSE"<<course;
     }
 };
 int main()
